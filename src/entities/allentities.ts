c21a24a1a68e8c802b9fca2a39f47ce747400007
ahmedishtiaq9777 <ahmedishtiaq9777@gmail.com>
2025-10-08 14:58:40 +0100

    name change

--- src/entities/allentities.ts
+++ src/entities/allentities.ts
@@ -1,4 +1,4 @@
-export interface User {
+export interface Usertype {
   id: number;
   name: string;
   email: string;
@@ -6,17 +6,17 @@ export interface User {
   created_at?: string;
 }
 
-export interface Task {
+export interface Tasktype {
   id: number;
   title: string;
   description?: string;
   scheduled_for?: string;
-  priority?: "low" | "medium" | "high";
+  priority?: "Low" | "Moderate" | "Extreme";
   status?: "pending" | "in_progress" | "completed";
   user_id: number;
 }
 
-export interface Notification {
+export interface Notificationtype {
   id: number;
   message: string;
   is_read: boolean;
